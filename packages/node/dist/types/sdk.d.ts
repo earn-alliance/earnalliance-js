e4d953a4e6ab89877e8b39382ecb1228963a49fe
wood <yfleung3@gmail.com>
2023-10-25 16:17:07 +0800

    feat: publish workflow

--- packages/node/dist/types/sdk.d.ts
+++
@@ -1,4 +0,0 @@
-import { NodeClient } from './client';
-import type { NodeOptions } from './types';
-export declare function init(options?: NodeOptions): NodeClient;
-//# sourceMappingURL=sdk.d.ts.map
--- packages/node/dist/types/track.d.ts
+++
@@ -1,3 +0,0 @@
-import type { IEvent, ITraits } from './types';
-export declare function track(userId: string, eventName: string, valueOrTraits?: number | ITraits, traits?: ITraits): IEvent;
-//# sourceMappingURL=track.d.ts.map
